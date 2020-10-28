17392b7a3748297506cbe03e93cb3f4cf4f1a3f2
Berkeley19 <berkeleyvh20@gmail.com>
2020-10-28 18:37:19 +0000

    Updates

--- precache-manifest.7e6b03bc4a53db6cf891d49c3c458977.js
+++ precache-manifest.7e6b03bc4a53db6cf891d49c3c458977.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "af5ce5c62faf166965d0eefc7a93e325",
+    "revision": "99377a1ef3db0945752206d632039517",
     "url": "/berkeley19.github.io/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/berkeley19.github.io/static/css/2.7eb84643.chunk.css"
   },
   {
-    "revision": "c701ea90076938bd7058",
+    "revision": "2f6a57de4baff617ea11",
     "url": "/berkeley19.github.io/static/css/main.b1f0936e.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/berkeley19.github.io/static/js/2.de0ac4f9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c701ea90076938bd7058",
-    "url": "/berkeley19.github.io/static/js/main.7874c7a5.chunk.js"
+    "revision": "2f6a57de4baff617ea11",
+    "url": "/berkeley19.github.io/static/js/main.15918ace.chunk.js"
   },
   {
     "revision": "98c3353364e1af2c0a31",
