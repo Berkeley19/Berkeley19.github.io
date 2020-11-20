575de275522e56de067980651aadd5ec96e81889
Berkeley19 <berkeleyvh20@gmail.com>
2020-11-20 21:21:35 +0000

    installations

---
+++ build/precache-manifest.bcc63c6961d1914034e208e713c80015.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0013f5f0ef6b2967af1a92c5401af20",
+    "url": "/berkeley19.github.io/index.html"
+  },
+  {
+    "revision": "293c3b85e722fdb5b978",
+    "url": "/berkeley19.github.io/static/css/2.7eb84643.chunk.css"
+  },
+  {
+    "revision": "96fcd69422e912813f14",
+    "url": "/berkeley19.github.io/static/css/main.b1f0936e.chunk.css"
+  },
+  {
+    "revision": "293c3b85e722fdb5b978",
+    "url": "/berkeley19.github.io/static/js/2.a80c9d53.chunk.js"
+  },
+  {
+    "revision": "e9875d7c1187f4f0558e7fbac051f48d",
+    "url": "/berkeley19.github.io/static/js/2.a80c9d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96fcd69422e912813f14",
+    "url": "/berkeley19.github.io/static/js/main.84f084b7.chunk.js"
+  },
+  {
+    "revision": "98c3353364e1af2c0a31",
+    "url": "/berkeley19.github.io/static/js/runtime-main.bc554358.js"
+  },
+  {
+    "revision": "1b3eb47ea33f2464408ef6260615d9e5",
+    "url": "/berkeley19.github.io/static/media/Berkeley_Vago_Hughes_CV_Resume.1b3eb47e.pdf"
+  },
+  {
+    "revision": "3695dfd885eee889848b9ed14104bcc1",
+    "url": "/berkeley19.github.io/static/media/aUnixPic.3695dfd8.jpg"
+  },
+  {
+    "revision": "530c19f7e07d182c750958609b2497d3",
+    "url": "/berkeley19.github.io/static/media/bkgrdCode.530c19f7.webp"
+  },
+  {
+    "revision": "b14082e684354932355042b9350f2534",
+    "url": "/berkeley19.github.io/static/media/endSF.b14082e6.jpg"
+  },
+  {
+    "revision": "bd0e6c79110a70f55fc2ebfef790cf57",
+    "url": "/berkeley19.github.io/static/media/f1.bd0e6c79.jpg"
+  },
+  {
+    "revision": "4c894ffd8b887ec15172cbdeeda22132",
+    "url": "/berkeley19.github.io/static/media/f2.4c894ffd.jpg"
+  },
+  {
+    "revision": "94f4fa3a5e7ad7c575a5607b83df21bd",
+    "url": "/berkeley19.github.io/static/media/f3.94f4fa3a.jpg"
+  },
+  {
+    "revision": "5457d10e8d681cb43d095db3d4c2538b",
+    "url": "/berkeley19.github.io/static/media/f4.5457d10e.jpg"
+  },
+  {
+    "revision": "dcd57e2957cc4894c98ac1f90e3a3da5",
+    "url": "/berkeley19.github.io/static/media/f5.dcd57e29.jpg"
+  },
+  {
+    "revision": "75f6ef8008df19d237f85734b4779844",
+    "url": "/berkeley19.github.io/static/media/f6.75f6ef80.jpg"
+  },
+  {
+    "revision": "13ec480bd9271381197ec5eaa9732148",
+    "url": "/berkeley19.github.io/static/media/fFinal.13ec480b.jpg"
+  },
+  {
+    "revision": "677084c2f6d3356e22606ec5469e1a0e",
+    "url": "/berkeley19.github.io/static/media/finalOutlinepp.677084c2.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/berkeley19.github.io/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/berkeley19.github.io/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/berkeley19.github.io/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/berkeley19.github.io/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/berkeley19.github.io/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "05acfdb568b3df49ad31355b19495d4a",
+    "url": "/berkeley19.github.io/static/media/ionicons.05acfdb5.woff"
+  },
+  {
+    "revision": "24712f6c47821394fba7942fbb52c3b2",
+    "url": "/berkeley19.github.io/static/media/ionicons.24712f6c.ttf"
+  },
+  {
+    "revision": "2c2ae068be3b089e0a5b59abb1831550",
+    "url": "/berkeley19.github.io/static/media/ionicons.2c2ae068.eot"
+  },
+  {
+    "revision": "621bd386841f74e0053cb8e67f8a0604",
+    "url": "/berkeley19.github.io/static/media/ionicons.621bd386.svg"
+  },
+  {
+    "revision": "f70a0e384e88820b1930920b51675cdf",
+    "url": "/berkeley19.github.io/static/media/ppCnew.f70a0e38.png"
+  },
+  {
+    "revision": "e61d912726c9cbc299d60ad089b490b4",
+    "url": "/berkeley19.github.io/static/media/r1.e61d9127.jpg"
+  },
+  {
+    "revision": "731de7bbf3cb52341c012dec1f430217",
+    "url": "/berkeley19.github.io/static/media/r2.731de7bb.jpg"
+  },
+  {
+    "revision": "ec85908f3f07aed2177b7a7ac9cbb9a0",
+    "url": "/berkeley19.github.io/static/media/r3.ec85908f.jpg"
+  },
+  {
+    "revision": "9efda318822b2c4eb5650289a95e214f",
+    "url": "/berkeley19.github.io/static/media/r4.9efda318.jpg"
+  },
+  {
+    "revision": "a55bd433c5ec5c32bbb46a7091fb5017",
+    "url": "/berkeley19.github.io/static/media/sf1.a55bd433.jpg"
+  },
+  {
+    "revision": "2d14c670c9d97b9af62ed0829e9d2202",
+    "url": "/berkeley19.github.io/static/media/sf2.2d14c670.jpg"
+  },
+  {
+    "revision": "5afeee7f6649b2fdca19ee88558fe888",
+    "url": "/berkeley19.github.io/static/media/sf3.5afeee7f.jpg"
+  },
+  {
+    "revision": "929512153d2b1f1c949f96e7b83df376",
+    "url": "/berkeley19.github.io/static/media/sf5.92951215.jpg"
+  },
+  {
+    "revision": "d6765beb4595f0f71c9131c04e9149d9",
+    "url": "/berkeley19.github.io/static/media/sf6.d6765beb.jpg"
+  }
+]);
